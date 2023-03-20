aaa13aefc2a72454470054d6a022147ed728bceb
Monika Maurya <96534851+monikamaurya77@users.noreply.github.com>
2023-03-20 09:56:48 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import {Link, useNavigate} from 'react-router-dom';
 import { useUserAuth } from '../context/UserAuthContext';
 import GoogleButton from 'react-google-button';
